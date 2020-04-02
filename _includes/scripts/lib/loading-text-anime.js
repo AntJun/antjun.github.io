5a9f5350b68a37faa3e0f800fa638411f6a8148e
unknown <AntJun_2016@outlook.com>
2020-04-02 20:39:23 +0800

    Alpha v1.7.3
    - 修复 "Back to top" 与  "Tidio Talk" 按钮在页面缩小时重叠的问题
    - 暂时取消图片 Lazyload 模式（为修复文章搜索功能）
    - 更新所有网易云音乐播放器至 Aplayer 播放
    - Aplayer 播放优化（1. 歌词添加 2. 主题颜色更改 3. 多余文件删除）
    - 添加部分代码的注释
    - "光标追踪（非正规）"，"Loading text anime" JS 代码移动至单独文件
    - 删除顶端进度指示器
    - 载入 color-thief.js

---
+++ _includes/scripts/lib/loading-text-anime.js
@@ -0,0 +1,44 @@
+
+/* LOADING TEXT ANIME */
+
+var speed = "1000" // in ms 1000ms = 1s
+var timer;
+var con = "";
+var c = 0;
+var text = document.getElementById("loadingText").textContent;
+var letters = text.split("");
+
+//convert text to letters , and add each letter in span
+function textToLetters(t) {
+  for (i = 0; i < t.length; i++) {
+    con += "<span id='" + i + "' class='extra' >" + t[i] + "</span>";
+  }
+  document.getElementById("loadingText").innerHTML = con;
+}
+
+function startAnime(id) {
+  if (c < letters.length) {
+    timer = setTimeout(function() {
+      if (c == 0) {
+        document.getElementById("" + (letters.length - 1) + "").classList.remove("anime");
+      }
+      if (c > 0) {
+        document.getElementById("" + (c - 1) + "").classList.remove("anime");
+      }
+      document.getElementById("" + c + "").classList.add("anime");
+      c++
+      startAnime();
+    }, speed);
+  } else {
+    c = 0;
+    startAnime();
+  }
+}
+
+//stop animetion
+function stopAnime() {
+  clearTimeout(timer);
+}
+
+textToLetters(letters);
+startAnime();
