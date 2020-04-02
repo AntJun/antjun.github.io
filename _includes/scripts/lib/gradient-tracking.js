872c76242ad0a3bc560f3ef68d690f57ee9918dc
unknown <AntJun_2016@outlook.com>
2020-04-02 20:39:23 +0800

    Alpha v1.7.3
    - 修复 "Back to top" 与  "Tidio Talk" 按钮在页面缩小时重叠的问题
    - 暂时取消图片 Lazyload 模式（为修复文章搜索功能）
    - 更新所有网易云音乐播放器至 Aplayer 播放
    - Aplayer 播放优化（1. 歌词添加 2. 主题颜色更改 3. 多余文件删除）
    - 添加部分代码的注释
    - "光标追踪（非正规）"，"Loading text anime" JS 代码移动至单独文件
    - 删除顶端进度指示器
    - 载入 color-thief.js

---
+++ _includes/scripts/lib/gradient-tracking.js
@@ -0,0 +1,8 @@
+/* 鼠标光标梯度跟踪 */
+var btn = document.querySelector('.mouse-cursor-gradient-tracking')
+btn.onmousemove = function(e) {
+  var x = e.pageX - btn.offsetLeft
+  var y = e.pageY - btn.offsetTop
+  btn.style.setProperty('--x', x + 'px')
+  btn.style.setProperty('--y', y + 'px')
+}
